ccafc3493666b48c1981e8c78c1d3d69eb2e67b8
Florian Straubinger <git@f-str.de>
2023-12-21 10:22:46 +0100

    Added checks to verify the needed programs are indeed installed

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,14 @@ mod util;
 mod cli;
 
 fn main() {
-    commands::parse_arguments();
+    if !util::check_if_program_is_installed("fzf") {
+        println!("fzf is not installed");
+        std::process::exit(1);
+    }
+    if !util::check_if_program_is_installed("rg") {
+        println!("ripgrep is not installed");
+        std::process::exit(1);
+    }
+
+    cli::parse_arguments();
 }
--- src/util.rs
+++ src/util.rs
@@ -8,7 +8,12 @@ pub fn read_from_file(path: &str) -> io::Result<String> {
 }
 
 pub fn write_to_file(path: &str, content: &str) -> io::Result<()> {
-    match File::create(Path::new(expand_path(path.to_string()).as_str())) {
+    let binding = expand_path(path.to_string());
+    let path = Path::new(binding.as_str());
+    if !path.exists() {
+        std::fs::create_dir_all(path.parent().unwrap())?;
+    }
+    match File::create(path) {
         Ok(mut file) => match file.write_all(content.as_bytes()) {
             Ok(_) => Ok(()),
             Err(err) => Err(err),
@@ -17,10 +22,18 @@ pub fn write_to_file(path: &str, content: &str) -> io::Result<()> {
     }
 }
 
-fn expand_path(path: String) -> String {
+pub fn expand_path(path: String) -> String {
     if path.starts_with('~') {
         let home = std::env::var("HOME").unwrap();
         return path.replacen('~', home.as_str(), 1);
     }
     path
+}
+
+pub fn check_if_program_is_installed(program: &str) -> bool {
+    let output = std::process::Command::new("which")
+        .arg(program)
+        .output()
+        .expect("Failed to execute command");
+    output.status.success()
 }
